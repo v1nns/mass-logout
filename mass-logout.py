35ec37c8fd46bfdfbfbdc891115abbae3059b385
Vinicius Longaray <vinicius.longaray@datacom.ind.br>
2019-08-12 14:43:06 -0300

    Added first version without the logout

---
+++ mass-logout.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import cookiemanager;
+
+if __name__ == "__main__":
+    cj = cookiemanager.chrome()
+    # for cookie in cj:
+    #     print ('name', cookie.name, 'value', cookie.value, 'domain', cookie.domain)
